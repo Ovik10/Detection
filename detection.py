4f84d59c9d347ed617f791710dd2e5dff3ee17c5
Ovik10 <svobodajindrich@centrum.cz>
2023-12-13 11:18:18 +0100

    zvýšení efektivity HOG + SVM

--- detection.py
+++ detection.py
@@ -1,15 +1,53 @@
+
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.utils import to_categorical
-from skimage import exposure
+from skimage import exposure, color
 from skimage.feature import hog
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from joblib import Parallel, delayed
+import time
 
 # načtení CIFAR-10 datasetu
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+def extract_single_hog(image):
+    gray_image = color.rgb2gray(image)
+    fd, _ = hog(gray_image, orientations=8, pixels_per_cell=(4, 4), cells_per_block=(1, 1), visualize=True)
+    return fd
+
+def extract_hog_features_parallel(images):
+    start_time = time.time()
+    hog_features = Parallel(n_jobs=-1)(delayed(extract_single_hog)(image) for image in images)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f'HOG extraction time: {elapsed_time:.2f} seconds')
+    return hog_features
+# Extraktování HOG funkcí pro trénování a testování
+train_images_hog_parallel = extract_hog_features_parallel(train_images)
+test_images_hog_parallel = extract_hog_features_parallel(test_images)
+
+# Flatten HOG funkce
+train_images_hog_flat = [img.flatten() for img in train_images_hog_parallel]
+test_images_hog_flat = [img.flatten() for img in test_images_hog_parallel]
+
+# Flatten labels pro SVM
+train_labels_flat = train_labels.flatten()
+test_labels_flat = test_labels.flatten()
+
+# Trénování HOG + SVM modelu
+svm_model = SVC()
+svm_model.fit(train_images_hog_flat, train_labels_flat)
+
+# Predikování s HOG + SVM modelem
+predictions_hog = svm_model.predict(test_images_hog_flat)
+
+# Evaluace HOG + SVM modelu
+accuracy_hog = accuracy_score(test_labels_flat, predictions_hog)
+print(f'HOG + SVM Test accuracy: {accuracy_hog}')
 
 # předpřipravování dat pro CNN
 train_images_cnn, test_images_cnn = train_images / 255.0, test_images / 255.0
@@ -37,36 +75,3 @@ model_cnn.fit(train_images_cnn, train_labels_cnn, epochs=10, validation_data=(te
 # Evaluace CNN modelu
 test_loss_cnn, test_acc_cnn = model_cnn.evaluate(test_images_cnn, test_labels_cnn)
 print(f'CNN Test accuracy: {test_acc_cnn}')
-
-# Předpřipravení dat pro HOG + SVM
-def extract_hog_features(images):
-    hog_features = []
-    for image in images:
-        gray_image = tf.image.rgb_to_grayscale(image).numpy()[:, :, 0]
-        fd, hog_image = hog(gray_image, orientations=8, pixels_per_cell=(4, 4), cells_per_block=(1, 1), visualize=True)
-        hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
-        hog_features.append(fd)
-    return hog_features
-
-# Extraktování HOG funkcí pro trénování a testování
-train_images_hog = extract_hog_features(train_images)
-test_images_hog = extract_hog_features(test_images)
-
-# Flatten HOG funkce
-train_images_hog_flat = [img.flatten() for img in train_images_hog]
-test_images_hog_flat = [img.flatten() for img in test_images_hog]
-
-# Flatten labels pro SVM
-train_labels_flat = train_labels.flatten()
-test_labels_flat = test_labels.flatten()
-
-# Trénování HOG + SVM modelu
-svm_model = SVC()
-svm_model.fit(train_images_hog_flat, train_labels_flat)
-
-# Predikování s HOG + SVM modelem
-predictions_hog = svm_model.predict(test_images_hog_flat)
-
-# Evaluace HOG + SVM modelu
-accuracy_hog = accuracy_score(test_labels_flat, predictions_hog)
-print(f'HOG + SVM Test accuracy: {accuracy_hog}')
